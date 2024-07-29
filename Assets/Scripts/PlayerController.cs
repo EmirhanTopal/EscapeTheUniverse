a19b49f2ed9af3d040a486c4c94c9ca8a1ffc4e8
Emirhan Topal <emirhantopal03@hotmail.com>
2024-07-29 23:02:24 +0300

    changed folder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrough = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrough);
-        
-        float verticalThrough = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrough);
+        float horizontalThroughX = movement.ReadValue<Vector2>().x;
+        float verticalThroughY = movement.ReadValue<Vector2>().y;
+
+
+        // float horizontalThrough = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalThroughX);
+        //
+        // float verticalThrough = Input.GetAxis("Vertical");
+        Debug.Log(verticalThroughY);
     }
 }
