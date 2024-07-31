e959146b152861b4dc5621ba77794ba1ef81a1f8
Emirhan Topal <emirhantopal03@hotmail.com>
2024-07-31 23:14:41 +0300

    ship controller developing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] InputAction movement;
     [SerializeField] private float xSpeed;
     [SerializeField] private float ySpeed;
+
+    [SerializeField] [Range(0, 50)] private float clampedXValue;
+    [SerializeField] [Range(0, 50)] private float clampedYValue;
+
+    private float horizontalThroughX, verticalThroughY;
+
+    [SerializeField] private float pitchPositionValue;
+    [SerializeField] private float pitchControlFactor;
+
+    [SerializeField] private float yawRotationFactor;
+
+    [SerializeField] private float rollControlFactor;
     
     private void OnEnable()
     {
@@ -24,22 +36,34 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MovementSpaceShip();
+        RotationSpaceShip();
     }
 
+    void RotationSpaceShip()
+    {
+        float pitch = transform.localPosition.y * pitchPositionValue + verticalThroughY * pitchControlFactor; // uçak aşağı yönde giderken uçağın burun aşağı bakar. x ROTASYON değeri bu duruma gönde değişir - x ekseni etrafında dönmek - (-) değer olacak
+        float yaw = transform.localPosition.x * yawRotationFactor;
+        float roll = horizontalThroughX * rollControlFactor; // uçak sağa veya sola giderken uçağın kanatları sağa veya sola yatar. z ROTASYON değeri bu duruma göre değişir - z ekseni etrafında dönmek - (-) değer olacak 
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+        Debug.Log(roll);
+    }
+    
     private void MovementSpaceShip()
     {
         //LocalPosition bir nesnenin kendi parent'ına göre konumunu ifade eder.
         
-        float horizontalThroughX = movement.ReadValue<Vector2>().x;
-        float verticalThroughY = movement.ReadValue<Vector2>().y;
+        horizontalThroughX = movement.ReadValue<Vector2>().x;
+        verticalThroughY = movement.ReadValue<Vector2>().y;
 
         float xOffset = xSpeed * horizontalThroughX * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(newXPos, -clampedXValue, clampedXValue);
         
         float yOffset = ySpeed * verticalThroughY * Time.deltaTime;
         float newYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(newYPos, -clampedYValue + 5, clampedYValue);
         
         //transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);  *** yanlış *** normal pozisyon
-        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
