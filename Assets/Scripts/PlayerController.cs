6d549719fd576e0a3f9cefbd9e2976b545595454
Emirhan Topal <emirhantopal03@hotmail.com>
2024-08-04 16:50:49 +0300

    attributes learned

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,33 +6,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
+    //[SerializeField] InputAction movement;
+    
+    [Header("Control Speed")]
+    [Tooltip("How fast ship moves up and down based on player input")]
     [SerializeField] private float xSpeed;
     [SerializeField] private float ySpeed;
-
+    
+    [Header("Clamped values")]
     [SerializeField] [Range(0, 50)] private float clampedXValue;
     [SerializeField] [Range(0, 50)] private float clampedYValue;
 
-    private float horizontalThroughX, verticalThroughY;
-
+    private float _horizontalThroughX, _verticalThroughY;
+    
+    [Header("Pitch")]
+    [Tooltip("Screen position based tuning")]
     [SerializeField] private float pitchPositionValue;
+    [Tooltip("Control factor for player input")]
     [SerializeField] private float pitchControlFactor;
-
+    
+    [Header("Yaw")]
+    [Tooltip("Screen position based tuning")]
     [SerializeField] private float yawPositionFactor;
-
+    
+    [Header("Roll")]
+    [Tooltip("Control factor for player input")]
     [SerializeField] private float rollControlFactor;
-
+    
+    [Header("Laser features")]
     [SerializeField] private GameObject[] lasers;
      
-    private void OnEnable()
-    {
-        movement.Enable();
-    }
-
-    private void OnDisable()
-    {
-        movement.Disable();
-    }
+    // private void OnEnable()
+    // {
+    //     movement.Enable();
+    // }
+    //
+    // private void OnDisable()
+    // {
+    //     movement.Disable();
+    // }
 
     // Update is called once per frame
     void Update()
@@ -44,9 +56,9 @@ public class PlayerController : MonoBehaviour
 
     void RotationSpaceShip()
     {
-        float pitch = transform.localPosition.y * pitchPositionValue + verticalThroughY * pitchControlFactor; // uçak aşağı yönde giderken uçağın burun aşağı bakar. x ROTASYON değeri bu duruma gönde değişir - x ekseni etrafında dönmek - (-) değer olacak
+        float pitch = transform.localPosition.y * pitchPositionValue + _verticalThroughY * pitchControlFactor; // uçak aşağı yönde giderken uçağın burun aşağı bakar. x ROTASYON değeri bu duruma gönde değişir - x ekseni etrafında dönmek - (-) değer olacak
         float yaw = transform.localPosition.x * yawPositionFactor;
-        float roll = horizontalThroughX * rollControlFactor; // uçak sağa veya sola giderken uçağın kanatları sağa veya sola yatar. z ROTASYON değeri bu duruma göre değişir - z ekseni etrafında dönmek - (-) değer olacak 
+        float roll = _horizontalThroughX * rollControlFactor; // uçak sağa veya sola giderken uçağın kanatları sağa veya sola yatar. z ROTASYON değeri bu duruma göre değişir - z ekseni etrafında dönmek - (-) değer olacak 
         transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
         Debug.Log(roll);
     }
@@ -59,14 +71,14 @@ public class PlayerController : MonoBehaviour
         // horizontalThroughX = movement.ReadValue<Vector2>().x;
         // verticalThroughY = movement.ReadValue<Vector2>().y;
 
-        horizontalThroughX = Input.GetAxis("Horizontal");
-        verticalThroughY = Input.GetAxis("Vertical");
+        _horizontalThroughX = Input.GetAxis("Horizontal");
+        _verticalThroughY = Input.GetAxis("Vertical");
         
-        float xOffset = xSpeed * horizontalThroughX * Time.deltaTime;
+        float xOffset = xSpeed * _horizontalThroughX * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(newXPos, -clampedXValue, clampedXValue);
         
-        float yOffset = ySpeed * verticalThroughY * Time.deltaTime;
+        float yOffset = ySpeed * _verticalThroughY * Time.deltaTime;
         float newYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(newYPos, -clampedYValue + 12, clampedYValue);
         
